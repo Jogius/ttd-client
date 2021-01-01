5d1c2eee91fc9886df6f2f8a2c7b3bb9ac49fa1b
Jogius <jogius.julius.makowski@gmail.com>
2021-01-01 14:23:41 +0100

    feat: v-message component

---
+++ components/v-message.vue
@@ -0,0 +1,34 @@
+<template>
+  <div :class="{ 'd-flex flex-row-reverse': message.me }">
+    <v-card
+      class="message-container pa-4 mb-2 text-wrap"
+      :color="message.me ? 'primary' : 'secondary'"
+      elevation="6"
+    >
+      <span class="message-content">
+        {{ message.content }}
+      </span>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.message-container {
+  border-radius: 15px;
+  max-width: 60%;
+}
+.message-content {
+  pointer-events: none;
+}
+</style>
